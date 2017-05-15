895a00913b28805e6771fad2b85035ca86b758c5
Sajith Sudharshana <ssgunathilaka@gmail.com>
2017-05-15 23:41:38 +0530

    joined three tables to retrieve transaction data

--- routes/admin.php
+++ routes/admin.php
@@ -6,3 +6,5 @@ CRUD::resource('tag', 'tagCrudController');
 CRUD::resource('message', 'messageCrudController');
 
 CRUD::resource('audit', 'auditCrudController');
+
+CRUD::resource('transaction', 'transactionCrudController');
